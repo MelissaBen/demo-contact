9aee27ca06f0f2f3ee7f92496e7780a21bfe23c4
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2020-07-08 17:12:52 +0200

    maj

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,29 +12,38 @@
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
 
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
+    <form
+      name="contact"
+      method="post"
+      v-on:submit.prevent="handleSubmit"
+      action="/success/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
         <label>
-          Your Name:
-          <input type="text" name="name" />
+          Don’t fill this out:
+          <input name="bot-field" />
         </label>
       </p>
-      <p>
-        <label>
-          Your Email:
-          <input type="email" name="email" />
-        </label>
-      </p>
+      <div class="sender-info">
+        <div>
+          <label for="name" class="label">Your name</label>
+          <input type="text" name="name" v-model="formData.name" />
+        </div>
+        <div>
+          <label for="email">Your email</label>
+          <input type="email" name="email" v-model="formData.email" />
+        </div>
+      </div>
 
-      <p>
-        <label>
-          Message:
-          <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      <div class="message-wrapper">
+        <label for="message">Message</label>
+        <textarea name="message" v-model="formData.message"></textarea>
+      </div>
+
+      <button type="submit">Submit form</button>
     </form>
   </Layout>
 </template>
@@ -44,17 +53,32 @@ export default {
   metaInfo: {
     title: "Hello, world!"
   },
-  name: "QAForm",
-  methods: {
-    updatePanelist(ev) {
-      this.currentPanelist = ev.target.value;
-    }
-  },
+
   data() {
     return {
-      panelists: ["Evan You", "Chris Fritz"],
-      currentPanelist: "Evan You"
+      formData: {}
     };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    },
+    handleSubmit(e) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({
+          "form-name": e.target.getAttribute("name"),
+          ...this.formData
+        })
+      })
+        .then(() => this.$router.push("/success"))
+        .catch(error => alert(error));
+    }
   }
 };
 </script>
