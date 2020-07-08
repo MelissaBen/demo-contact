5b063523bc7dffdfe0279774f88a423ce54eca53
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2020-07-08 16:42:24 +0200

    first c

---
+++ src/pages/Index.vue
@@ -0,0 +1,62 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name:
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
